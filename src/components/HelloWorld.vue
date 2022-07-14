13fca3fff9c06bc60923c3fac8eb52085384e7e7
liaohongze <liaohongze@163.com>
2022-07-15 00:28:06 +0800

    增加tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="text-blue-100">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
@@ -17,9 +17,9 @@ defineProps<{
 
 <style scoped>
 h1 {
+  top: -10px;
   font-weight: 500;
   font-size: 2.6rem;
-  top: -10px;
 }
 
 h3 {
