4bdc54bfba0d06e5503267da537d4ac3e08968a3
liaohongze <liaohongze@163.com>
2022-07-14 20:48:39 +0800

    增加stylelint配置

---
+++ .stylelintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-recommended-vue',
+    'stylelint-config-recommended-vue/scss',
+    'stylelint-config-rational-order'
+  ],
+  customSyntax: 'postcss-html',
+  rules: {
+    indentation: 2,
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep']
+      }
+    ],
+    'number-leading-zero': 'never',
+    'no-descending-specificity': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'selector-type-no-unknown': null,
+    'at-rule-no-unknown': null,
+    'no-duplicate-selectors': null,
+    'no-empty-source': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      { ignorePseudoClasses: ['global'] }
+    ]
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -26,10 +26,10 @@ import HelloWorld from './components/HelloWorld.vue'
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
-  line-height: 1.5;
   max-height: 100vh;
+  line-height: 1.5;
 }
 
 .logo {
@@ -39,9 +39,9 @@ header {
 
 nav {
   width: 100%;
+  margin-top: 2rem;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -80,12 +80,11 @@ nav a:first-of-type {
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
     margin-top: 1rem;
+    margin-left: -1rem;
+    padding: 1rem 0;
+    font-size: 1rem;
+    text-align: left;
   }
 }
 </style>
