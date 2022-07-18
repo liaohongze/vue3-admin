140329d67b94966bd5d1372b0525085021216930
liaohongze <liaohongze@163.com>
2022-07-18 17:21:04 +0800

    更改检查配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,15 +8,12 @@ module.exports = {
   customSyntax: 'postcss-html',
   rules: {
     indentation: 2,
-    'selector-class-pattern': '^([a-z][a-z0-9]*)(-+[a-z0-9]+)*$',
-
     'selector-pseudo-element-no-unknown': [
       true,
       {
         ignorePseudoElements: ['v-deep']
       }
     ],
-    'number-leading-zero': 'never',
     'no-descending-specificity': null,
     'font-family-no-missing-generic-family-keyword': null,
     'selector-type-no-unknown': null,
