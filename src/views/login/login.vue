206a9d4a07bd795fd2cfb9f247f9aef0ba353b54
liaohongze <liaohongze@163.com>
2022-07-19 17:47:23 +0800

    写登录页

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -8,7 +8,66 @@
       /></a>
     </div>
 
-    <div>login body</div>
+    <div
+      class="flex flex-row w-1000 mx-auto rounded-10 overflow-hidden c-box-shadow3 sm:w-full sm:mt-20"
+    >
+      <div class="w-420 sm:hidden">
+        <img
+          v-for="(item, index) in loginBanner"
+          :key="index"
+          alt=""
+          class="w-full h-fulll"
+          :src="item.imgUrl"
+        />
+      </div>
+
+      <div class="mx-auto w-400 px-10 sm:w-80p">
+        <ContactUs v-if="contactUsVisible" v-model="contactUsVisible" />
+
+        <template v-else>
+          <div class="flex flex-row justify-center items-center pt-35 mb50">
+            <div
+              v-for="(item, index) in loginTypeOptions"
+              :key="index"
+              class="cursor-pointer"
+              :class="{
+                'mr-30': index < loginTypeOptions.length - 1,
+                'text-18 c-fc-sgray leading-28': loginType != item.type,
+                'text-24 c-fc-333 leading-36 font-medium':
+                  loginType == item.type
+              }"
+              @click="loginTypeChange(item)"
+            >
+              {{ item.title }}
+            </div>
+          </div>
+        </template>
+
+        <div class="flex flex-row items-center justify-between mb-10 text-14">
+          <div class="flex flex-row items-center">
+            <span class="ml-5 text-14">阅读并同意</span>
+          </div>
+
+          <div class="flex flex-row">
+            <div class="text-center c-fc-666">
+              <span
+                v-if="loginType === 'password'"
+                class="c-fc-blue cursor-pointer"
+                @click="modifyPassword"
+              >
+                忘记密码
+              </span>
+            </div>
+
+            <span
+              class="c-fc-blue cursor-pointer"
+              @click="contactUsVisible = !contactUsVisible"
+              >联系我们</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div class="text-center c-fc-sgray text-14">
       <p class="pb-23">-创客匠人 让商业更智能-</p>
@@ -16,3 +75,40 @@
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref, reactive } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+import { imgWebUrl } from '@/constants/global'
+import ContactUs from './part/contactUs.vue'
+
+const loginBanner = reactive([
+  {
+    imgUrl: `${imgWebUrl}/assets/loginBanner/banner.jpg`,
+    value: 1
+  }
+])
+
+// const loadCaptcha = () => {
+
+// }
+
+const loginType = ref<string>('password')
+const loginTypeOptions = reactive([
+  { type: 'password', title: '密码登录' },
+  { type: 'code', title: '验证码登录' }
+])
+
+const loginTypeChange = item => {
+  // loadCaptcha()
+  loginType.value = item.type
+}
+
+const modifyPassword = () => {
+  router.push({ name: 'modifyPassword' })
+}
+
+const contactUsVisible = ref<boolean>(false)
+</script>
---
+++ src/views/login/part/contactUs.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    class="relative w-full h-400 mt-15 mb-20 text-center rounded-10 c-box-shadow4"
+  >
+    <img
+      src="@/assets/logo/wechatLogo.png"
+      class="absolute top-0 right-0"
+      width="150"
+      alt=""
+    />
+
+    <span class="absolute contact-close pointer" @click="close"
+      ><i class="el-icon-close text-24 c-fc-666"></i
+    ></span>
+
+    <p class="pt-60 mb-30 text-24 leading-30 c-fc-333 font-medium">联系我们</p>
+
+    <div class="c-bd1-ccc w-160 h-160 mx-auto">
+      <img
+        :src="`${imgWebUrl}/assets/loginBanner/contactUs.jpg`"
+        class="w-full"
+        alt=""
+      />
+    </div>
+    <div class="text-14 c-fc-333 mt-20 font-medium leading-20">
+      请打开“<span class="c-fc-blue">企业微信</span>”，使用扫一扫
+      <p>添加上面客服进行沟通</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { imgWebUrl } from '@/constants/global'
+
+type Props = {
+  contactUsVisible: boolean
+}
+defineProps<Props>()
+
+const emit = defineEmits(['update:modelValue'])
+
+const close = () => {
+  emit('update:modelValue', 1)
+}
+</script>
+
+<style lang="scss" scoped>
+.contact-us::after {
+  position: absolute;
+  bottom: -20px;
+  left: 307px;
+  display: block;
+  width: 0;
+  height: 0;
+  overflow: hidden;
+  font-size: 0;
+  line-height: 0;
+  border: 10px;
+  border-color: #fff transparent transparent;
+  border-style: solid dashed dashed;
+  content: '';
+}
+</style>
