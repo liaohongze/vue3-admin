b45bac8bcfcb86597fa16ce729d19f4b3e0bebbd
liaohongze <liaohongze@163.com>
2022-07-20 18:44:12 +0800

    关闭ts对any的校验

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
     '@vue/eslint-config-prettier'
   ],
   rules: {
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-explicit-any': ['off']
   }
 }
