3010385b5ee451e32bf17c165a5402a7afca607c
liaohongze <liaohongze@163.com>
2022-08-01 18:22:43 +0800

    子组件自动更新的hook

--- src/hooks/autoSetProps.ts
+++ src/hooks/autoSetProps.ts
@@ -1,16 +1,25 @@
-import { reactive } from 'vue'
+import { toRaw } from 'vue'
 
-export function objectParams(
-  propName: string,
-  obj: Record<string, any>,
-  emit: any
-) {
+export function objectParams(reactiveObj: Record<string, any>) {
+  const obj = toRaw(reactiveObj)
   return new Proxy(obj, {
     get: function (target, propKey: string) {
-      return obj[propKey]
+      return reactiveObj[propKey]
     },
-    set: function (target, propKey, value, receive) {
-      emit(`update:${propName}`, reactive({ ...target, [propKey]: value }))
+    set: function (target, propKey: string, value, receive) {
+      reactiveObj[propKey] = value
+      return Reflect.set(target, propKey, value, receive)
+    }
+  })
+}
+
+export function baseParams(obj: Record<string, any>, props: any, emit: any) {
+  return new Proxy(obj, {
+    get: function (target, propKey: string) {
+      return props[propKey]
+    },
+    set: function (target, propKey: string, value, receive) {
+      emit(`update:${propKey}`, value)
       return Reflect.set(target, propKey, value, receive)
     }
   })
